39905f8d17b0384b7a398a9ac8a58668330dae9c
DUVAN100 <duvanvivas100@gmail.com>
2022-07-05 20:27:09 -0500

    add files

--- curs_udemy_js/Exersices_Practics/exercise2_acordionContent/app.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll('.acordeon .item .header button');
-
-items.forEach(item => {
-    item.addEventListener('click', e => {
-        const content = e.target.parentElement.nextElementSibling;
-        content.classList.toggle('show');
-    });
-});
