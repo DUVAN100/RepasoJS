0075ed69e11166ad2bb4c2e8445b89b660b00c42
DUVAN100 <duvanvivas100@gmail.com>
2022-08-03 22:27:03 -0500

    add files

---
+++ CRUDjs/app.js
@@ -0,0 +1,47 @@
+let el, i;
+let data = [
+    {firstname: "mauricio", lastname: "sevilla"},
+    {firstname: "gorge", lastname: "baron"},
+    {firstname: "maria", lastname: "posada"},
+    {firstname: "leticia", lastname: "marin"}
+];
+let panel = document.querySelector("#panel");
+
+//aqui limiamos los campos
+function clearform(){
+    document.querySelector("#fname").value= "";
+    document.querySelector("#Iname").value= "";
+}
+function renderItem(){
+    panel.textContent= "";
+    data.forEach(x =>{
+        el = document.createElement("option");
+        el.innerText = `${x.firstname} ${x.lastname}`;
+        panel.append(el);
+    });
+}
+
+function create(){
+    let fn = document.querySelector("#fname").value;
+    let In = document.querySelector("#Iname").value;
+    data = [...data, {firstname: fn, lastname: In }];
+    clearform();
+    renderItem();
+}
+function panelClick(){
+    i = panel.selectedIndex;
+    document.querySelector("#fname").value = data[i].firstname;
+    document.querySelector("#Iname").value = data[i].lastname;
+}
+function update(){
+    data[i].firstname = document.querySelector("#fname").value;
+    data[i].lastname = document.querySelector("#Iname").value;
+    clearform();
+    renderItem();
+}
+function deleteItem(){
+    data.splice(i, 1);
+    renderItem();
+    clearform();
+}
+renderItem();
